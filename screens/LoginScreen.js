fadfce257d3c175b3285bde87f2dff95cb40acb0
YJ Soon <yjsoon@tinkertanker.com>
2020-12-18 10:10:09 +0800

    Styled up the login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -9,8 +9,52 @@ import {
 
 export default function LoginScreen({ navigation }) {
   return (
-    <View>
-      <Text>Hello this is login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Chat App</Text>
+      <Text style={styles.fieldTitle}>Email</Text>
+      <TextInput style={styles.input} />
+      <Text style={styles.fieldTitle}>Password</Text>
+      <TextInput style={styles.input} />
+      <TouchableOpacity onPress={null} style={styles.loginButton}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  input: {
+    borderColor: "#999",
+    borderWidth: 1,
+    marginBottom: 24,
+    padding: 4,
+    height: 36,
+    fontSize: 18,
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    width: 120,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
